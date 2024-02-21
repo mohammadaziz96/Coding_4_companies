addd20a669a91657194e77aa7cec5cc48ba48f37
Mohammad Aziz <aziznitham@gmail.com>
2024-02-21 07:50:40 +0000

    Created using Colaboratory

--- samsung/two_splitting_words.py
+++ samsung/two_splitting_words.py
@@ -1,11 +1,39 @@
-def split_word(list1):
-    word, seq=list1
-    word_list=seq.split(",")
-    print(word_list)
-    for i in word_list:
-        print(i, end="")
-        if  word.startswith(i) in word_list:
-            return word[len(i):]
-
-
-print(split_word(["baseball", "a,ab,abc,b,ba,bas,base,bc,bce,ball,d,de,dec"]))
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyN464rjh8Q2SC8tA26Ihn6r",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/mohammadaziz96/Coding_4_companies/blob/main/samsung/two_splitting_words.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "k0dTwAPRocHl"
+      },
+      "outputs": [],
+      "source": []
+    }
+  ]
+}
